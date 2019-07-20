cb234229f99d02b9d4e3a8a2ba408dfd98af455e
sneha <sneha.meshram@cgi.com>
2019-07-20 19:46:03 +0530

    update task

--- src/main/java/com/stackroute/task/PrinterMain.java
+++ src/main/java/com/stackroute/task/PrinterMain.java
@@ -4,10 +4,15 @@ public class PrinterMain {
     public static void main(){
         Printing printer=new Printing();
         printer.Printer(80,6,4,100);
+
         printer.getTonerLevel();
+
         printer.getNumberOfPagesPrint();
+
         printer.getBothside();
+
         printer.getNumberOfPages();
+
         printer.printPage(10);
 
 
